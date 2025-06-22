aa81387cc8334b1eacb06100cd7cd2f1a7767166
mmkit <ataichukuev3@gmail.com>
2025-06-22 10:56:00 +0500

    add service

---
+++ src/main/java/com/example/project_telegram_bot/service/BuildingUrlService.java
@@ -0,0 +1,25 @@
+package com.example.project_telegram_bot.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Service
+public class BuildingUrlService {
+    @Value("${url.translate-website}")
+    private String translationUrl;
+    private final String SOURCE_LANG = "en"; // Язык оригинала
+    private final String TARGET_LANG = "ru";
+
+
+    public String getTranslationUrl(String text) {
+
+        String url = UriComponentsBuilder.fromUriString(translationUrl)
+                .queryParam("q", text)
+                .queryParam("langpair", SOURCE_LANG + "|" + TARGET_LANG)
+                .build()
+                .toUriString();
+
+        return url;
+    }
+}
