e537df4501466edad14e2336d87d8e1d958c7514
mmkit <ataichukuev3@gmail.com>
2025-06-22 10:56:05 +0500

    add service

---
+++ src/main/java/com/example/project_telegram_bot/service/RequestService.java
@@ -0,0 +1,33 @@
+package com.example.project_telegram_bot.service;
+
+import com.example.project_telegram_bot.entity.ResponseData;
+import com.example.project_telegram_bot.entity.TranslationResponse;
+import com.example.project_telegram_bot.error.RequestServiceException;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@Service
+public class RequestService {
+    private final RestTemplate restTemplate;
+
+    public RequestService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    public String getTranslatedText(String url) throws RequestServiceException {
+        try {
+            ResponseEntity<TranslationResponse> response = restTemplate.getForEntity(url, TranslationResponse.class);
+            TranslationResponse translationResponse = response.getBody();
+            if (translationResponse != null && translationResponse.getResponseData() != null) {
+                return translationResponse.getResponseData().getTranslatedText();
+            }
+            throw new RequestServiceException("Не удалось получить переведенный текст");
+        } catch (Exception e) {
+            throw new RequestServiceException("Ошибка во время запроса: ", e);
+        }
+    }
+}
