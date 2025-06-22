d023f674d4dc28431ad49603ff5421610308081f
mmkit <ataichukuev3@gmail.com>
2025-06-22 10:56:50 +0500

    add exceptions

---
+++ src/main/java/com/example/project_telegram_bot/error/RequestServiceException.java
@@ -0,0 +1,12 @@
+package com.example.project_telegram_bot.error;
+
+public class RequestServiceException extends RuntimeException {
+
+  public RequestServiceException(String message) {
+    super(message);
+  }
+
+  public RequestServiceException(String message, Throwable cause) {
+    super(message, cause);
+  }
+}
---
+++ src/main/java/com/example/project_telegram_bot/error/TranslatorServiceException.java
@@ -0,0 +1,12 @@
+package com.example.project_telegram_bot.error;
+
+public class TranslatorServiceException extends RuntimeException {
+
+    public TranslatorServiceException(String message) {
+        super(message);
+    }
+
+    public TranslatorServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
