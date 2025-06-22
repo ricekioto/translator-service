63d3351855fef6ae7559ab5ac354b1839a691091
mmkit <ataichukuev3@gmail.com>
2025-06-22 08:02:25 +0500

    commit project

---
+++ src/main/java/com/example/project_telegram_bot/TranslatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_telegram_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranslatorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranslatorServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/project_telegram_bot/TranslatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_telegram_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TranslatorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
