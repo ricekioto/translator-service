5092ef1de20c38786f950ae24490c70cee367c89
mmkit <ataichukuev3@gmail.com>
2025-06-22 10:56:35 +0500

    add entity

---
+++ src/main/java/com/example/project_telegram_bot/entity/ResponseData.java
@@ -0,0 +1,13 @@
+package com.example.project_telegram_bot.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResponseData {
+    @JsonProperty("translatedText")
+    private String translatedText;
+
+}
