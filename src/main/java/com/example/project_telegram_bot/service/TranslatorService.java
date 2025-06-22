14c5a276fdb526c5d343a218dee6508e9b8a287b
mmkit <ataichukuev3@gmail.com>
2025-06-22 10:56:11 +0500

    add service

---
+++ src/main/java/com/example/project_telegram_bot/service/TranslatorService.java
@@ -0,0 +1,21 @@
+package com.example.project_telegram_bot.service;
+
+import com.example.project_telegram_bot.entity.ResponseData;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TranslatorService {
+    private final BuildingUrlService buildingUrlService;
+    private final RequestService requestService;
+
+    public TranslatorService(BuildingUrlService buildingUrlService, RequestService requestService) {
+        this.buildingUrlService = buildingUrlService;
+        this.requestService = requestService;
+    }
+
+    public String getTranslatedText(String text) {
+        String url = buildingUrlService.getTranslationUrl(text);
+        String response = requestService.getTranslatedText(url);
+        return response;
+    }
+}
