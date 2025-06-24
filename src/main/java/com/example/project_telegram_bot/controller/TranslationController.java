a494d6360ecf0262e1be1464d12b052336f44deb
mmkit <ataichukuev3@gmail.com>
2025-06-24 12:21:50 +0500

    review

--- src/main/java/com/example/project_telegram_bot/controller/TranslationController.java
+++ src/main/java/com/example/project_telegram_bot/controller/TranslationController.java
@@ -2,21 +2,17 @@ package com.example.project_telegram_bot.controller;
 
 import com.example.project_telegram_bot.error.TranslatorServiceException;
 import com.example.project_telegram_bot.service.TranslatorService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api")
 public class TranslationController {
 
     private final TranslatorService translatorService;
-
-    @Autowired
-    public TranslationController(TranslatorService translatorService) {
-        this.translatorService = translatorService;
-    }
 
     @GetMapping("/translate")
     public String translate(@RequestParam String text) throws TranslatorServiceException {
