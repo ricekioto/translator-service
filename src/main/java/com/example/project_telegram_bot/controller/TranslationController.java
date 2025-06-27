8b74673cec8eac8472f9f40dcf0807e71c63cf59
mmkit <ataichukuev3@gmail.com>
2025-06-27 20:31:04 +0500

    review

--- src/main/java/com/example/project_telegram_bot/controller/TranslationController.java
+++ src/main/java/com/example/project_telegram_bot/controller/TranslationController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api")
 public class TranslationController {
-
     private final TranslatorService translatorService;
 
     @GetMapping("/translate")
